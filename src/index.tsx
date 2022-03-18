add21556073e4a0f53aee95328f86634718696b7
William Li <email@williamli.dev>
2022-03-18 17:54:49 +0800

    fix typo in basename

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ import { Footer } from '@ui/footer';
 render(
   <ThemeModeProvider
     render={(mode) => (
-      <Router basename="/demo/73360-react-router-motion-demo">
+      <Router basename="/demos/73360-react-router-motion-demo">
         <ThemeProvider theme={theme(mode)}>
           <GlobalStyles>
             <MotionConfigProvider>
